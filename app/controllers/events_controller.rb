d92df114365494f507140c697f18d1cabddf375d
Raghu Betina <raghu@firstdraft.com>
2017-03-05 19:16:40 +0000

    Automatically associate comment to signed in user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -56,8 +56,6 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-
-    @event.user_id = params[:user_id]
     @event.restaurant_id = params[:restaurant_id]
     @event.host = params[:host]
     @event.title = params[:title]
