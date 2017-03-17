bdad66277ea11e6a79a837376fd529a764a4145f
K-Wei <k-wei@users.noreply.github.com>
2017-03-17 04:38:04 -0500

    set default user reservation/attendance count to 0 (instead of nil)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @hosted_count = @user.events_count
+    if @hosted_count == nil
+       @hosted_count = 0
+     end
+    if @user.reservations_count == nil
+       @user.reservations_count = 0
+    end
     @attended_count = @user.reservations_count - @hosted_count
   end
 
